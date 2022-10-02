85fbe9f0c0fcfe860128aaf44a26f5df031c1815
Ratibor Živanović <zivanovicratibor@gmail.com>
2022-10-02 22:25:49 +0200

    Basic form

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const PWA = require('next-pwa')
+
+module.exports = PWA({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+})
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
+
+// module.exports = nextConfig
